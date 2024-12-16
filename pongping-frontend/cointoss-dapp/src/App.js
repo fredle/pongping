3aad0fb1b74b6f23ee37b86da5241146c4ad7416
Freddie Leatham <freddie@leatham.com>
2024-12-16 17:18:19 +0000

    Small updates

--- pongping-frontend/cointoss-dapp/src/App.js
+++ pongping-frontend/cointoss-dapp/src/App.js
@@ -6,17 +6,17 @@ import { BrowserProvider, parseUnits } from "ethers";
 
 // Import from a specific export
 import { HDNodeWallet } from "ethers/wallet";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import CoinTossABI from './CoinTossABI.json';
 import { Tabs, Tab, Box, Typography } from '@mui/material';
 import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Button } from '@mui/material';
 
 
 
-const contractAddress = '0x50bC93420B345836EC9abac7AA76A2d01F43E7bd'; //base
+const contractAddress = '0x201964bE15DbEEa4F7f8E2919C491ab01abB58Bb'; //base
 //const contractAddress = '0xf029Bc092a0EC2F550c8C72c5A2D81fF39c3d265'; //sepolia
 //const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3'; //local
-
+//const contractAddress = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512'
 
 function TabPanel({ children, value, index }) {
   return (
@@ -39,6 +39,8 @@ function App() {
     const [message, setMessage] = useState("");
     const [value, setValue] = useState(0);
 
+    const signerAddressRef = useRef("No Account Connected");
+    const contractRef = useRef(null);
 
     
     const [open, setOpen] = useState(false);
@@ -51,75 +53,94 @@ function App() {
     const handleLoss = () => {
       setDialogMessage("You lost the coin toss. Better luck next time!");
       setOpen(true);
-      getPlayerBalance();
     };
 
     const handleWin = (winnings) => {
       setDialogMessage(`You won ${ethers.formatUnits(winnings, 'ether')} ETH! Congratulations!`);
       setOpen(true);
-      getPlayerBalance();
     };
 
 
     const handleChange = (event, newValue) => {
       setValue(newValue);
     };
+
+    const coinTossResultHandler = (player, result, winnings) => {
+      if(player !== signerAddressRef.current) {
+        console.log(`${player} played, but not you! Coin toss result: ${result}`);
+        refreshBalances();
+        return;
+      }
+      console.log(`Coin toss result: ${result}`);
+      console.log(`Winnings: ${ethers.formatUnits(winnings, 'ether')} ETH`);
+      //getPlayerBalance();
+      if (result === 'Tails') {
+        handleLoss();
+      } else {
+        handleWin(winnings);
+      }
+      refreshBalances();
+    }
+
+    const depositHandler = (user, amount) => {
+      if(user !== signerAddressRef.current) {
+        console.log(`${user} deposited ${amount}, but not you!`);
+        refreshBalances();
+        return;
+      }
+      console.log(`Deposit event: ${user} deposited ${ethers.formatUnits(amount, 'ether')} ETH`);
+      const depositMessage = `Deposit event: ${user} deposited ${ethers.formatUnits(amount, 'ether')} ETH`;
+      setMessage(depositMessage);
+      setOpen(true);
+      refreshBalances();
+    }
+
     useEffect(() => {
-        const initializeProvider = async () => {
-            let signer = null;
+      const initializeProvider = async () => {
+        let signer = null;
 
-            let provider;
-            if (window.ethereum == null) {
+        let provider;
+        if (window.ethereum == null) {
+      
+          console.log("MetaMask not installed; using read-only defaults")
+          provider = ethers.getDefaultProvider()
+      
+        } else {
+          provider = new ethers.BrowserProvider(window.ethereum)
+
+          try {
+            signer = await provider.getSigner();
             
-                console.log("MetaMask not installed; using read-only defaults")
-                provider = ethers.getDefaultProvider()
-            
+            const signerAddress = await signer.getAddress();
+            signerAddressRef.current = signerAddress;
+            setSignerAddress(signerAddress);
+            const contract = new ethers.Contract(contractAddress, CoinTossABI, signer);
+            setSigner(signer);
+            setContract(contract);
+            contractRef.current = contract;
+            console.log(signerAddress);
+
+            contract.on('CoinTossResult', coinTossResultHandler);
+            contract.on('Deposit', depositHandler);
+            refreshBalances();
+          } catch (error) {
+            console.log(error.code);
+            if (error.code === -32002) {
+              console.error('User is not logged in to MetaMask');
             } else {
-                provider = new ethers.BrowserProvider(window.ethereum)
-
-                try {
-                  signer = await provider.getSigner();
-                  
-                  setSignerAddress(await signer.getAddress())
-                  console.log(signerAddress);
-                  const contract = new ethers.Contract(contractAddress, CoinTossABI, signer);
-                  setSigner(signer);
-                  setContract(contract);
-
-                  contract.on('CoinTossResult', (player, result, winnings) => {
-                    console.log(`Coin toss result: ${result}`);
-                    console.log(`Winnings: ${ethers.formatUnits(winnings, 'ether')} ETH`);
-                    //getPlayerBalance();
-                    if (result === 'Tails') {
-                      handleLoss();
-                    } else {
-                      handleWin(winnings);
-                    }
-                  });
-                  contract.on('Deposit', (user, amount) => {
-                    console.log(`Deposit event: ${user} deposited ${ethers.formatUnits(amount, 'ether')} ETH`);
-                    const depositMessage = `Deposit event: ${user} deposited ${ethers.formatUnits(amount, 'ether')} ETH`;
-                    setMessage(depositMessage);
-                  });
-                } catch (error) {
-                  console.log(error.code);
-                  if (error.code === -32002) {
-                    console.error('User is not logged in to MetaMask');
-                  } else {
-                    console.error('Error connecting to MetaMask:', error);
-                  }
-                }
+              console.error('Error connecting to MetaMask:', error);
             }
+          }
+        }
+      };
 
-        };
-
-        initializeProvider();
+      initializeProvider();
     }, []);
 
 
     const getContractBalance = async () => {
       try {
-        const balance = await contract.getContractBalance();
+        const balance = await contractRef.current.getContractBalance();
         console.log(balance);
         const balanceInEth = ethers.formatUnits(balance, 'ether');
         console.log('Contract balance in ETH:', balanceInEth);
@@ -143,6 +164,7 @@ function App() {
         const tx = await contract.depositFunds({ value: ethers.parseUnits(amount, 'ether') });
         await tx.wait();
         console.log('Deposit successful:', tx);
+        refreshBalances();
         setDialogMessage("Deposit successful");
         setOpen(true);
 
@@ -160,6 +182,7 @@ function App() {
         const tx = await contract.withdraw(ethers.parseUnits(amount, 'ether'));
         await tx.wait();
         console.log('Withdrawal successful:', tx);
+        refreshBalances();
         setDialogMessage(`Withdrawal successful`);
         setOpen(true);
 
@@ -176,7 +199,11 @@ function App() {
       setDialogMessage("Flipping the coin! please accept the Wallet Transaction");
       setOpen(true);
       try {
-        const tx = await contract.coinToss(ethers.parseUnits(betAmount, 'ether'));
+        const gasEstimate = await contract.coinToss.estimateGas(ethers.parseUnits(betAmount, 'ether'));
+        console.log('Gas estimate:', (gasEstimate+5000n));
+        const gasLimit = gasEstimate + 5000n; // Increase gas limit slightly above the estimate
+        const tx = await contract.coinToss(ethers.parseUnits(betAmount, 'ether'), { gasLimit });
+
         await tx.wait();
         console.log('Coin toss transaction successful:', tx);
         setOpen(false);
@@ -197,6 +224,7 @@ function App() {
         const tx = await contract.playerWithdraw(ethers.parseUnits(amount, 'ether'));
         await tx.wait();
         console.log('Player withdrawal successful:', tx);
+        refreshBalances();
         setDialogMessage("Player withdrawal successful");
         setOpen(true);
       } catch (error) {
@@ -212,6 +240,8 @@ function App() {
         const tx = await contract.playerWithdrawAll();
         await tx.wait();
         console.log('Player withdrawal of all funds successful:', tx);
+        refreshBalances();
+
         setDialogMessage("Player withdrawal of all funds successful");
         setOpen(true);
 
@@ -224,7 +254,7 @@ function App() {
     };
     const getPlayerBalance = async () => {
       try {
-        const balance = await contract.getPlayerBalance();
+        const balance = await contractRef.current.getPlayerBalance();
         const balanceInEth = ethers.formatUnits(balance, 'ether');
 
         setPlayerBalance(balanceInEth);
@@ -237,13 +267,31 @@ function App() {
 
       }
     };
+    const refreshBalances = async () => {
+      getContractBalance();
+      getPlayerBalance();
+    };
+
+    const listAllPlayers = async () => {
+      try {
+        const [addresses, balances] = await contractRef.current.listAllPlayers();
+        const players = addresses.map((address, index) => ({
+          address,
+          balance: ethers.formatUnits(balances[index], 'ether')
+        }));
+        console.log(players);
+      } catch (error) {
+        const errorMessage = error.reason || error.message.split(':').pop().trim();
+        console.error('Error fetching player balance:', error);
+      }
+    };
+
 
     return (
       <div style={{ height: '100vh', overflow: 'hidden' }}>
       <h1>CoinToss DApp</h1>
 
       <div>Account: {signerAddress}</div>
-
 
       <div>
         <div>Player Balance: {playerBalance}ETH<button onClick={getPlayerBalance}>🔄</button></div>
@@ -306,7 +354,10 @@ function App() {
           />
           <button onClick={() => withdrawFunds(deposit)}>Withdraw</button>
         </div>
-
+        <div>
+          <h2>List All Players</h2>
+          <button onClick={() => listAllPlayers()}>List All Players</button>
+        </div>
         </Box>
       </TabPanel>
 
